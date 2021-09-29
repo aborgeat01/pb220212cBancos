764c1af520d3c641c856468904e9ad7bd8b7aa69
unknown <aborgeat@unlam.edu.ar>
2021-09-29 11:19:46 -0300

    elimine equals decliente y modique cuenta

--- src/ar/edu/unlam/bancos/Cliente.java
+++ src/ar/edu/unlam/bancos/Cliente.java
@@ -29,35 +29,7 @@ public class Cliente {
 	public void setSexo(Character sexo) {
 		this.sexo = sexo;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
-		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		if (dni == null) {
-			if (other.dni != null)
-				return false;
-		} else if (!dni.equals(other.dni))
-			return false;
-		if (sexo == null) {
-			if (other.sexo != null)
-				return false;
-		} else if (!sexo.equals(other.sexo))
-			return false;
-		return true;
-	}
+	
 
 
 }
